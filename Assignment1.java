7bea127fcc7cca433c78e34384d086a2846da093
Chiaoan <feel8012@hotmail.com>
2015-03-03 18:01:03 +0800

    Assignments4week1
    assignment

---
+++ Assignment1.java
@@ -0,0 +1,10 @@
+class Assignment1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("《芙蓉樓送辛漸》作者：王昌齡\n");
+		System.out.println("寒雨連江夜入吳，平明送客楚山孤。\n");
+		System.out.println("洛陽親友如相問，一片冰心在玉壺。\n");
+		
+	}
+}
